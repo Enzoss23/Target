945ac627b88541e3938b6c0abd351afc1584f4d1
Enzo Santos Souza <92988290+Enzoss23@users.noreply.github.com>
2023-02-25 02:14:06 -0300

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,16 @@
+function fibonacci (num){
+    let fib = [0, 1];
+    
+
+    while(fib[fib.length - 1] < num){
+        fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+    }
+    
+    if (fib.includes(num)){
+        console.log (`O número ${num} pertence à sequência de Fibonacci.`);
+    } else {
+        console.log (`O número ${num} não pertence à sequência de Fibonacci.`);
+    }
+}
+fibonacci(22);
+fibonacci(3);
