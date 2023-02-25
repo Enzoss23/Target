bc91073642b08b076fa47e233c99276278b54c0d
Enzo Santos Souza <92988290+Enzoss23@users.noreply.github.com>
2023-02-25 02:20:45 -0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,16 +1,17 @@
 function fibonacci (num){
     let fib = [0, 1];
     
-
+// a sequência de fibonacci
     while(fib[fib.length - 1] < num){
         fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
     }
-    
+ // verificação se o pertence a sequência ou não   
     if (fib.includes(num)){
         console.log (`O número ${num} pertence à sequência de Fibonacci.`);
     } else {
         console.log (`O número ${num} não pertence à sequência de Fibonacci.`);
     }
 }
-fibonacci(22);
-fibonacci(3);
+//chamando a função para rodar a sequência e averificação
+fibonacci(22);//número que não pertence a sequência
+fibonacci(3);//número que pertence a sequência
